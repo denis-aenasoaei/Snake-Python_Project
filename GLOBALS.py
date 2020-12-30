b92070373cb87e2b9b6a6c1106fcd4461f689552
Denis <denis.aenasoaei@yahoo.com>
2020-12-31 00:11:07 +0200

    added GLOBALS file with general game settings

---
+++ GLOBALS.py
@@ -0,0 +1,16 @@
+screen_width = 480
+screen_height = 480
+
+grid_size = 20
+
+block_width = screen_width / grid_size
+block_height = screen_width / grid_size
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
+
+PLAYER = 1
+
+initial_size = 2
